f2668dfc6ec6bb158b6115de188301a0888ae9d0
unknown <jessicagualtieri@hotmail.fr>
2024-11-04 17:25:01 +0100

    ✨ Day one pt. 1 is completed

--- DayOne/DayOne/Program.cs
+++ DayOne/DayOne/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text.RegularExpressions;
 
-Console.WriteLine("Hello, World!");
+string file = "input.txt";
+string pathFile = AppDomain.CurrentDomain.BaseDirectory + "\\" + file;
+StreamReader streamReader = new StreamReader(pathFile);
+string? line;
+int total = 0;
+
+while (!streamReader.EndOfStream)
+{
+    line = streamReader.ReadLine();
+    char[] characters = line?.ToCharArray();
+    List<int> digitInLine = new List<int>();
+    foreach (char character in characters)
+    {
+        if (Char.IsDigit(character))
+        {
+            int integer = int.Parse(character.ToString());
+            digitInLine.Add(integer);
+        }
+    }
+
+    string firstDigit = digitInLine.First().ToString();
+    string lastDigit = digitInLine.Last().ToString();
+    string calibration = firstDigit + lastDigit;
+    total += int.Parse(calibration);
+}
+
+Console.WriteLine(total);
